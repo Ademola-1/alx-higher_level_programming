4385aa8431a59fe7cd3fd8374ffe713a0cad0503
Ademola-1 <lawalademola71@gmail.com>
2024-04-15 04:50:54 -0700

    Hot Fix

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for uni_code in range(ord('a'), ord('z') + 1):
-	print(chr(uni_code), end='')
+    print(chr(uni_code), end='')
--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for tens_digit in range(10):
-	for units_digit in range(tens_digit + 1, 10):
-		num = tens_digit * 10 + units_digit
-		print("{:02d}".format(num), end=', ' if num < 89 else '\n')
+    for units_digit in range(tens_digit + 1, 10):
+        num = tens_digit * 10 + units_digit
+        print("{:02d}".format(num), end=', ' if num < 89 else '\n')
