3cd4e405727845e6720363e7c9988bd434ea5078
Ademola-1 <lawalademola71@gmail.com>
2024-07-31 21:39:02 -0700

    Hot fix

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -4,18 +4,18 @@ const request = require('request');
 const id = process.argv[2];
 const url = 'https://swapi-api.hbtn.io/api/films/';
 request.get(url + id, function (error, res, body) {
-    if (error) {
+  if (error) {
+    console.log(error);
+  }
+  const data = JSON.parse(body);
+  const content = data.characters;
+  for (const i of content) {
+    request.get(i, function (error, res, body1) {
+      if (error) {
         console.log(error);
-    }
-    const data = JSON.parse(body);
-    const content = data.characters;
-    for (const i of content) {
-        request.get(i, function (error, res, body) {
-            if (error) {
-                console.log(error);
-            }
-            const data1 = JSON.parse(body1);
-            console.log(data1.name);
-        });
-    }
+      }
+      const data1 = JSON.parse(body1);
+      console.log(data1.name);
+    });
+  }
 });
--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,26 +2,26 @@
 
 const request = require('request');
 const movieId = process.argv[2];
-const API_URL = 'https://swapi-api-alx-tools.com/api/films/${movieId}/'
+const API_URL = 'https://swapi-api.hbtn.io/api/films/' + movieId;
 
 request(API_URL, (error, response, body) => {
-    if (error) {
-        console.log(error);
-    } else if (response.statusCode === 200) {
-        const filmData = JSON.parse(body);
-        const characterUrls = filmData.characters;
-        
-        characterUrls.forEach((characterUrl) => {
-            request(characterUrl, (charError, charResponse, charBody) => {
-                if (charerror) {
-                    console.error(charError);
-                } else if (charResponse.statusCode === 200) {
-                    const characterData = JSON.parse(charBody);
-                    console.log(charcterData.name);
-                }
-            });
-        });
-    } else {
-        console.log('An error occurred. Status code: ${response.statusCode}');
-    }
+  if (error) {
+    console.log(error);
+  } else if (response.statusCode === 200) {
+    const filmData = JSON.parse(body);
+    const characterUrls = filmData.characters;
+
+    characterUrls.forEach((characterUrl) => {
+      request(characterUrl, (charError, charResponse, charBody) => {
+        if (charError) {
+          console.error(charError);
+        } else if (charResponse.statusCode === 200) {
+          const characterData = JSON.parse(charBody);
+          console.log(characterData.name);
+        }
+      });
+    });
+  } else {
+    console.log(`An error occurred. Status code: ${response.statusCode}`);
+  }
 });
